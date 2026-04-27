dd74a53fa4ff3361c20dc766be9e785fccee08c8
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-27 09:58:53 -0500

    main.py

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ import sys
 from todo_manager import read_todo_file, write_todo_file
 
 try:
-    nombre_script = sys.argv[0]
+    if len(sys.argv) < 2:
+        raise IndexError("Insufficient arguments provided!")
     ruta_del_archivo = sys.argv[1]
-    comando = sys.argv[2]
-    elementos_de_lista = read_todo_file(ruta_del_archivo)
-    #print(f"Command-line arguments:\n{ruta_del_archivo}\n")
-    if comando == "view":
-        print("Task:")
-        for elemento in elementos_de_lista:
-            print(elemento)
-    
-except IndexError:
-    print("Insufficient arguments provided!")
-except ValueError:
-    print("Command not found!")
+    try:
+        accion_a_realizar = sys.argv[2]
+        #print(f"Command-line arguments:\n{ruta_del_archivo}\n")
+        if accion_a_realizar == "view":
+            elementos_de_lista = read_todo_file(ruta_del_archivo)
+            print("Task:")
+            for elemento in elementos_de_lista:
+                print(elemento)
+        else:
+            raise ValueError("Command not found!")
+    except IndexError:
+        pass
+except IndexError as error:
+    print(error)
+except ValueError as error:
+    print(error)
