c92919383beff0d8dd00dfb59a4bbe7ca7bb7649
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-26 21:19:41 -0500

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ try:
     ruta_del_archivo = sys.argv[1]
     print(f"Command-line arguments:\n{ruta_del_archivo}\n")
     elementos_de_lista = read_todo_file(ruta_del_archivo)
-    print("Task:\n")
+    print("Task:")
     for elemento in elementos_de_lista:
         print(elemento)
 except IndexError:
