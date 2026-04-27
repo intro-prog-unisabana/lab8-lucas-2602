918a1f63dd7d16dddeec8235834c6e89e1b5d079
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-26 20:51:47 -0500

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 """Laboratorio 8 - CLI del gestor de tareas."""
 
 # TODO: Implementar CLI según README.md
+import sys
+
+from todo_manager import read_todo_file, write_todo_file
+
--- todo_manager.py
+++ todo_manager.py
@@ -1,6 +1,4 @@
 """Laboratorio 8 - Módulo de persistencia para lista de tareas."""
-
-
 def read_todo_file(file_path):
     """Reads tasks from a file. Returns a list of tasks."""
     # TODO: Implementar manejo de FileNotFoundError según README.md
@@ -15,4 +13,6 @@ def read_todo_file(file_path):
 def write_todo_file(file_path, tasks):
     """Writes tasks to a file, one per line."""
     # TODO: Implementar escritura de tareas según README.md
-    raise NotImplementedError
+    with open(file_path, 'w') as file:
+        for elemento in tasks:
+            file.write(elemento + "\n")
