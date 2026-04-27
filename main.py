c1ef7054c165892e94bd433559b7b732659abb47
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-26 21:05:15 -0500

    main.py

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ from todo_manager import read_todo_file, write_todo_file
 try:
     nombre_script = sys.argv[0]
     ruta_del_archivo = sys.argv[1]
+    print(read_todo_file(ruta_del_archivo))
 except IndexError:
     print("Insufficient arguments provided!")
