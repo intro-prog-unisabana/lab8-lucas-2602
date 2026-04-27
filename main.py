6a9010726d8c8e61d5f4c4c8540d45517f433912
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-26 21:26:39 -0500

    main.py

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from todo_manager import read_todo_file, write_todo_file
 try:
     nombre_script = sys.argv[0]
     ruta_del_archivo = sys.argv[1]
-    print(f"Command-line arguments:\n{ruta_del_archivo}\n")
+    #print(f"Command-line arguments:\n{ruta_del_archivo}\n\nTask:")
     elementos_de_lista = read_todo_file(ruta_del_archivo)
-    print("Task:")
     for elemento in elementos_de_lista:
         print(elemento)
 except IndexError:
     print("Insufficient arguments provided!")
+except ValueError:
+    print("Command not found!")
