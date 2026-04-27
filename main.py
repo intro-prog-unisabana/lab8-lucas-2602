d928533ce4fc0ec42701846b315d6daea1d38beb
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-26 21:29:12 -0500

    main.py

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from todo_manager import read_todo_file, write_todo_file
 try:
     nombre_script = sys.argv[0]
     ruta_del_archivo = sys.argv[1]
-    #print(f"Command-line arguments:\n{ruta_del_archivo}\n\nTask:")
+    #print(f"Command-line arguments:\n{ruta_del_archivo}\n")
     elementos_de_lista = read_todo_file(ruta_del_archivo)
+    print("Task:")
     for elemento in elementos_de_lista:
         print(elemento)
 except IndexError:
