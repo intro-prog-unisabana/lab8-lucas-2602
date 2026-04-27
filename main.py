98a24dd75eb0151857659d7a135b30a5c870b97c
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-26 21:03:05 -0500

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 
 # TODO: Implementar CLI según README.md
 import sys
-
 from todo_manager import read_todo_file, write_todo_file
 
+try:
+    nombre_script = sys.argv[0]
+    ruta_del_archivo = sys.argv[1]
+except IndexError:
+    print("Insufficient arguments provided!")
