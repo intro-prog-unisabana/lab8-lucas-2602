2b9beeb3c65d60294cd32eccda698cb1274c0c72
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-27 11:07:04 -0500

    main.py

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import sys
 from todo_manager import read_todo_file, write_todo_file
 
 try:
+    if sys.argv[1] == "--help":
+        print("""Usage: python main.py <file_path> <command> [arguments]...
+
+Commands:
+  add "task"    - Add a task to the list.
+  remove "task" - Remove a task from the list.
+  view          - Display all tasks.
+
+Examples:
+  python main.py tasks.txt add "Buy groceries"
+  python main.py tasks.txt remove "Do laundry"
+  python main.py tasks.txt view
+  python main.py tasks.txt add "Call mom" remove "Take out trash" view""")
+        sys.exit()
     if len(sys.argv) < 2:
         raise IndexError("Insufficient arguments provided!")
     if len(sys.argv) < 3:
